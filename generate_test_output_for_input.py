c68ea4b7d2533bfa7843af55dbb09ddc722873e7
Sung Min Ha <sungminha@wustl.edu>
2021-04-01 11:04:41 -0500

    Fixed path to pretrained weights

--- generate_test_output_for_input.py
+++ generate_test_output_for_input.py
@@ -13,7 +13,7 @@ from model import create_xception_unet_n
 import nibabel as nib
 
 data_file_path = "/scratch/hasm/Data/Lesion/ATLAS_R1.1/train.h5"
-pretrained_weights_file = "/scratch/hasm/Data/Lesion/X-net_Test/X-Net/x_net_final_score.csv"
+pretrained_weights_file = "/scratch/hasm/Data/Lesion/X-net_Test/X-Net/fold_2/trained_final_weights.h5"
 input_shape = (224, 192, 1)
 sample_input = "/scratch/hasm/Data/Lesion/ATLAS_R1.1/Only_Data/Site1/031768/t01/031768_t1w_deface_stx.nii.gz"
 
