468a9798e3c19bcd443a6740f9bed29e941f8dc0
Sung Min Ha <sungminha@wustl.edu>
2021-04-01 10:30:23 -0500

    test for loading in and genreatnig seg

---
+++ generate_test_output_for_input.py
@@ -0,0 +1,20 @@
+"""
+testing an input
+"""
+import keras
+import numpy as np
+from model import create_xception_unet_n
+import nibabel as nib
+
+data_file_path = "/scratch/hasm/Data/Lesion/ATLAS_R1.1/train.h5"
+pretrained_weights_file = "/scratch/hasm/Data/Lesion/X-net_Test/X-Net/x_net_final_score.csv"
+input_shape = (224, 192, 1)
+sample_input = "/scratch/hasm/Data/Lesion/ATLAS_R1.1/Only_Data/Site1/031768/t01/031768_t1w_deface_stx.nii.gz"
+
+print("".join(["data_file_path: (", data_file_path, ")"]))
+
+model = create_xception_unet_n(input_shape=input_shape, pretrained_weights_file=pretrained_weights_file)
+
+img = nib.load(sample_input).get_fdata()
+print(np.shape(img))
+model.predict(img)
