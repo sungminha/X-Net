cb357129bf57bec418364a8255d54f57cfa8e757
Sung Min Ha <sungminha@wustl.edu>
2021-04-08 12:09:03 -0500

    removed wrongly typed line

--- main.py
+++ main.py
@@ -15,7 +15,6 @@ from data import create_train_date_generator, create_val_date_generator
 
 #data_file_path = '/data/data/ATLAS_h5/ATLAS.h5'
 data_file_path = "/scratch/hasm/Data/Lesion/ATLAS_R1.1/train.h5"
-output_
 pretrained_weights_file = None
 input_shape = (224, 192, 1)
 #input_shape = (43281, 233, 197)
