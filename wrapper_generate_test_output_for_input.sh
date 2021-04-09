01bab7c2231bafa8fab3fac8a593517cb249919d
Sung Min Ha <sungminha@wustl.edu>
2021-04-08 23:34:27 -0500

    more memory requirements, more walltime

--- wrapper_generate_test_output_for_input.sh
+++ wrapper_generate_test_output_for_input.sh
@@ -7,7 +7,7 @@
 #PBS -e /scratch/hasm/Data/Lesion/X-net_Test/logs/\${PBS_JOBNAME}_\${PBS_JOBID}.stderr  #### stderr default path
 #PBS -M sungminha@wustl.edu  #### email address to nofity with following options/scenarios
 #PBS -m abe ####abort, end notifications - see below lines for more options
-#PBS -l nodes=1:ppn=1,mem=16gb,walltime=4:00:00 #### 1 node, 1 processor, 1 gpu, 8GB of memory, 15 hours of wall time requests
+#PBS -l nodes=1:ppn=1,mem=32gb,walltime=24:00:00 #### 1 node, 1 processor, 1 gpu, 8GB of memory, 15 hours of wall time requests
 ############################## END OF DEFAULT EMBEDDED PBS COMMANDS #######################
 
 parent_dir="/home/hasm/comp_space/Data/Lesion/X-net_Test/X-Net_20210401_CompleteDataSet_3Folds";
